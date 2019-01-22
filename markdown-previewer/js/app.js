79ced16f5e4c452904a446321c907aa54feb28ae
anpa6741 <anpa6641@colorado.edu>
2019-01-21 23:23:08 -0700

    added regex logic

--- markdown-previewer/js/app.js
+++ markdown-previewer/js/app.js
@@ -42,12 +42,84 @@ class Previewer extends React.Component {
    }
 
    render() {
-     return (
-       <div className='previewer'>
-         <Header name='Previewer'/>
-         <p>{this.props.display}</p>
-       </div>
-     );
+
+     const markup = this.props.input.split('\n');
+     const boldRegex = /\*\*/;
+     const crossOutRegex = /~~/;
+     const linksRegex = /\[links\]/;
+     const singleTicksRegex = /(?<!`)`{1}(?!`)/;
+     const multiTicksRegex = /^`{3}/gim;
+     let items = [];
+
+     for (let i = 0; i < markup.length; i++) {
+
+        if (markup[i].startsWith('# ')) {
+          items.push(<h1 style={{borderBottom: "solid black 3px"}}>{markup[i].split('# ')[1]}</h1>);
+        }
+        else if(markup[i].startsWith('## ')) {
+            items.push(<h2 style={{borderBottom: "solid black 3px"}}>{markup[i].split('## ')[1]}</h2>);
+        }
+        else if(markup[i].startsWith('### ')) {
+          items.push(<h3 style={{paddingTop: 20}}>{markup[i].split('## ')[1]}</h3>);
+        }
+        else if(markup[i] == ""){
+          items.push(<br />)
+        }
+        else if(boldRegex.test(markup[i])) {
+          let word = markup[i].split('**');
+          let text = <p>
+                      {word[0]}
+                      <span style={{fontWeight: "bold"}}>{word[1].replace(/_/g,'')}</span>
+                      {word.slice(2)}
+                    </p>;
+          items.push(text);
+        } else if(crossOutRegex.test(markup[i])) {
+          let word = markup[i].split('~~');
+          let text = <p>
+                      {word[0]}
+                      <span style={{textDecoration: "line-through"}}>{word[1].replace(/~/g,'')}</span>
+                      {word.slice(2)}
+                    </p>;
+          items.push(text);
+        } else if(linksRegex.test(markup[i])) {
+            let word = markup[i].split('[links]');
+            /* parsing the url from the string */
+            let linkLoc = word[1].slice(word[1].indexOf('https'), word[1].indexOf(')'));
+            /* parsing remaining text from the given line */
+            let lastWord = word[1].split(')').slice(1);
+            let text =
+                  <div>
+                      <p>
+                        {word[0]}
+                        <a href={linkLoc} target="_blank">[links]</a>
+                        {lastWord}
+                       </p>
+                  </div>
+            items.push(text);
+        } else if(singleTicksRegex.test(markup[i])) {
+          let word = markup[i].split('`');
+          let text = <p>
+                  {word[0]}
+                  <span style={{ backgroundColor: "white"}}>{word[1]}</span>
+                  {word.slice(2)}
+                 </p>
+          items.push(text);
+        } else if (multiTicksRegex.test(markup[i])) {
+            let text = <span style={{ display: "block", whiteSpace: "pre", backgroundColor: "white"}}>{markup[i]}</span>
+            items.push(text);
+        }
+        else {
+          items.push(<p style={{whiteSpace: "pre"}}>{markup[i]}</p>);
+        }
+      }
+
+      return (
+
+         <div className='previewer'>
+           <Header name='Previewer'/>
+           {items}
+         </div>
+       );
    }
  }
 
@@ -65,26 +137,25 @@ class App extends React.Component {
   }
 
   handleChange(event){
-      console.log("Handle Change");
       this.setState({
         inputValue: event.target.value
    });
   }
 
   render() {
+
     return (
       <div>
         <div>
           <Editor textArea={this.state.inputValue} handleChange={this.handleChange} />
         </div>
         <div>
-          <Previewer display={this.state.inputValue}/>
+          <Previewer input={this.state.inputValue}/>
         </div>
       </div>
     );
   }
 }
-
 
 App.defaultProps = {
    textArea: `# Welcome to my React Markdown Previewer!
